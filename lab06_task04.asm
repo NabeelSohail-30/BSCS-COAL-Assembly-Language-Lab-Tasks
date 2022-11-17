9864922065ab4d335232b9d76150af072afba655
Nabeel <nabeel.sohail2630@outlook.com>
2022-11-18 00:26:04 +0500

    lab 06 act 4

---
+++ lab06_task04.asm
@@ -0,0 +1,33 @@
+title lab 06 act 04
+.model small
+.stack 100h
+.data
+.code
+start:
+    mov ax,@data    ;initialize data segment
+    mov ds,ax
+
+    mov cx, 5      ;set counter
+    mov ah,02h      ;set output function
+    mov dl, 31h       ;set output character
+
+    @LOOP:
+        int 21h
+        inc dl
+        push dx
+
+        mov dl,08h      ;backspace
+        int 21h
+        mov bx, 30h
+
+        @hold:
+            nop
+            dec bx
+            jnz @hold
+        
+        pop dx
+    loop @LOOP
+
+    mov ah,4ch  ;exit to dos
+    int 21h
+end start
